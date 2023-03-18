c97df0c4305523d81da9a236dc97958faebc18d1
rybinali <rybinaaaaa.a@gmail.com>
2023-03-18 21:07:50 +0100

    init

---
+++ pjv-lab04/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.pjv;
+
+public class BruteForceAttacker extends Thief {
+    
+    @Override
+    public void breakPassword(int sizeOfPassword) {
+        // write your code 
+    }
+    
+}
