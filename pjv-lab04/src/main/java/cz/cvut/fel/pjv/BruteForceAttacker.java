3af8f5fb4efe372429cc743eba7299242dec3ddd
rybinali <rybinaaaaa.a@gmail.com>
2023-03-19 16:33:34 +0100

    ready

--- pjv-lab04/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
+++ pjv-lab04/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
@@ -4,16 +4,9 @@ import java.util.Arrays;
 
 public class BruteForceAttacker extends Thief {
     public void iterate(int sizeOfPassword, char[] password, int counter, char[] letters) {
-//        System.out.println("LENGHT ->" + counter);
-//        System.out.println("STRING ->" + Arrays.toString(password));
+        if (isOpened()) return;
         if (counter == sizeOfPassword) {
-            System.out.println("УСЛОВИЕ ВЫПОЛНЕНО");
-            System.out.println("STRING ->" + Arrays.toString(password));
             tryOpen(password);
-            if (isOpened()) {
-                System.out.println("1 point -> " + Arrays.toString(password));
-                return;
-            }
             return;
         }
 
@@ -27,7 +20,6 @@ public class BruteForceAttacker extends Thief {
     public void breakPassword(int sizeOfPassword) {
         char[] password = new char[sizeOfPassword];
         char[] letters = this.getCharacters();
-        System.out.println(letters);
-        iterate(sizeOfPassword, password, 0, letters, false);
+        iterate(sizeOfPassword, password, 0, letters);
     }
 }
