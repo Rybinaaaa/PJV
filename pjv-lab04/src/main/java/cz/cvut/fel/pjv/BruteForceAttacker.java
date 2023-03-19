3e3a32bbefcfcc7f8eab94811fffce79353fe3f9
rybinali <rybinaaaaa.a@gmail.com>
2023-03-19 16:16:18 +0100

    1 try

--- pjv-lab04/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
+++ pjv-lab04/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
@@ -1,10 +1,33 @@
 package cz.cvut.fel.pjv;
 
+import java.util.Arrays;
+
 public class BruteForceAttacker extends Thief {
-    
+    public void iterate(int sizeOfPassword, char[] password, int counter, char[] letters) {
+//        System.out.println("LENGHT ->" + counter);
+//        System.out.println("STRING ->" + Arrays.toString(password));
+        if (counter == sizeOfPassword) {
+            System.out.println("УСЛОВИЕ ВЫПОЛНЕНО");
+            System.out.println("STRING ->" + Arrays.toString(password));
+            tryOpen(password);
+            if (isOpened()) {
+                System.out.println("1 point -> " + Arrays.toString(password));
+                return;
+            }
+            return;
+        }
+
+        for (char ch : letters) {
+            password[counter] = ch;
+            iterate(sizeOfPassword, password, counter + 1, letters);
+        }
+    }
+
     @Override
     public void breakPassword(int sizeOfPassword) {
-        // write your code 
+        char[] password = new char[sizeOfPassword];
+        char[] letters = this.getCharacters();
+        System.out.println(letters);
+        iterate(sizeOfPassword, password, 0, letters, false);
     }
-    
 }
--- pjv-lab04/src/main/java/cz/cvut/fel/pjv/Start.java
+++ pjv-lab04/src/main/java/cz/cvut/fel/pjv/Start.java
@@ -3,8 +3,12 @@ package cz.cvut.fel.pjv;
 public class Start {
 
     public static void main(String[] args) {
-        
-            Test hw = new Test();
-            hw.start();
+
+//        String password = "abcda";
+//        BruteForceAttacker attacker = new BruteForceAttacker();
+//        attacker.init(new char[]{'a', 'b', 'c', 'd'}, password);
+//        attacker.breakPassword(5);
+        Test hw = new Test();
+        hw.start();
     }
 }
